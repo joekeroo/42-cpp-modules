35a92b9f77fac4aa92410fa95367854f64f7b5ee
Joel king yan Hii <jhii@u82z05s02.42kl.edu.my>
2022-10-19 15:52:24 +0800

    cpp07

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,17 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:25:06 by jhii              #+#    #+#             */
-/*   Updated: 2022/10/19 15:51:45 by jhii             ###   ########.fr       */
+/*   Updated: 2022/10/19 15:50:15 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_HPP
-# define WHATEVER_HPP
+#ifndef iter_HPP
+# define iter_HPP
+
+# define RESET		"\033[0m"
+# define BLACK		"\033[30m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define MAGENTA	"\033[35m"
+# define CYAN		"\033[36m"
+# define WHITE		"\033[37m"
 
 # include <iostream>
 

d36100b1b32d2e21becc37e056bcf76d74ed3185
Joel king yan Hii <jhii@u82z05s02.42kl.edu.my>
2022-10-19 18:44:03 +0800

    cpp08

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 15:25:06 by jhii              #+#    #+#             */
+/*   Updated: 2022/10/19 18:43:37 by jhii             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# define RESET		"\033[0m"
+# define BLACK		"\033[30m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define MAGENTA	"\033[35m"
+# define CYAN		"\033[36m"
+# define WHITE		"\033[37m"
+
+# include <iostream>
+
+template <typename T> void easyfind(T arr, int i)
+{}
+
+#endif
